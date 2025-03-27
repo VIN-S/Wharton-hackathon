2d336ac8f4a17c45b26f0548464f82b6f83bc417
Sun Yuxuan <syx0628@gmail.com>
2025-03-26 23:40:57 -0400

    backend apis done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const readline = require('readline')
-const { generateResponse } = require('./controllers/openaiController')
+const { quizMateResponse, noteMateResponse, writeMateResponse } = require('./controllers/openaiController')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-rl.question('Input: \n', generateResponse)
+rl.question('Input: \n', writeMateResponse)
--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,20 +1,55 @@
+// Controller for all the backend interactions with openAI APIs
 const openai = require('../config/openaiConfig')
 
-const generateResponse = async (question) => {
+const noteMateResponse = async (lectureContent) => {
 
-    const desctiption = await openai.chat.completions.create({
-        model: "gpt-3.5.turbo",
+    const answer = await openai.chat.completions.create({
+        model: "gpt-4o",
         messages: [
             {
                 role: 'user',
-                content: 'Come up with a desciption for Youtube called ${question}'
+                content: 'Please help summarize the following content into bullet points. Content body: ' + lectureContent
             }
         ],
-        max_tokens: 100
+        max_tokens: 10000
     })
 
 
-    console.log(desctiption.data.choices[0].message)
+    console.log(answer.choices[0].message)
 }
 
-module.exports = { generateResponse }
+const quizMateResponse = async (lectureContent) => {
+
+    const answer = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: 'user',
+                content: 'Please help me create a pop quiz based on the following content. Content body: ' + lectureContent
+            }
+        ],
+        max_tokens: 10000
+    })
+
+
+    console.log(answer.choices[0].message)
+}
+
+const writeMateResponse = async (content) => {
+
+    const answer = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: 'user',
+                content: 'Please help to fix the grammar in the following content and provide detailed explanation. Content: ' + content
+            }
+        ],
+        max_tokens: 10000
+    })
+
+
+    console.log(answer.choices[0].message)
+}
+
+module.exports = { noteMateResponse, quizMateResponse, writeMateResponse }
